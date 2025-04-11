76871d31bc3f16adb9d44314a718f37416e37b4a
Hendrilalaina <hendrilalaina@gmail.com>
2025-04-11 19:46:36 +0300

    Save expense using formik and yup

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,28 @@
+interface Props {
+  options: string[];
+  name: string;
+  id: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  onBlur: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  label: string;
+  error?: string;
+  touched?: boolean;
+}
+
+const Dropdown = ({ options, label, name, id, value, onChange, onBlur, error, touched }: Props) => {
+  return (
+    <div className="mb-3">
+      <label htmlFor={id} className="form-label">
+        {label}
+      </label>
+      <select name={name} id={id} className="form-control" onChange={onChange} onBlur={onBlur}>
+        <option value={value}>Select category</option>
+        {options.map(option => <option key={option} value={option}>{option}</option>)}
+      </select>
+      {touched && error ? (<div className="text-danger fst-italic">Expense category is required!</div>) : null}
+    </div>
+  )
+}
+
+export default Dropdown;
